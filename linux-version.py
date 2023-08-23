7071ad8d4c67e025b35d18b06067fdb7aa03421a
Tixerz <petete1296@gamil.com>
2023-08-23 00:43:24 -0400

    remove method update

--- linux-version.py
+++ linux-version.py
@@ -9,16 +9,22 @@ class dns:
             pk.dump(self , f)
         
     def remove(self):
-        os.remove(self.name)
+        os.remove(f"dns-servers/{self.name}")
         with open("/etc/resolv.conf","r") as f:
             lines=f.readlines()
-            for l in lines :
-                if l != f"nameserver {self.ip}":
+            print(lines)
+        with open("/etc/resolv.conf","w")as f:
+            for l in lines:
+                if l != f'nameserver {self.ip}':
                     f.write(l)
 
     
     def write(self):
         with open("/etc/resolv.conf" , "w") as file :
             file.write(f'\nnameserver {self.ip}')
+            print(f'\nnameserver {self.ip}')
 
 
+a = dns("sex" , "123")
+a.save()
+a.remove()
