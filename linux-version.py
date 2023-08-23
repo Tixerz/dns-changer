5831a64c9e7b621c13e2a1852f8c1b14f1921c90
Tixerz <petete1296@gamil.com>
2023-08-23 12:32:45 -0400

    console update

--- linux-version.py
+++ linux-version.py
@@ -23,5 +23,17 @@ class dns:
         with open("/etc/resolv.conf" , "w") as file :
             file.write(f'\nnameserver {self.ip}')
             print(f'\nnameserver {self.ip}')
+a = dns("google dns" , "8.8.8.8")
+a.save()
+obj_list = []
+for item in os.listdir("dns-servers/") :
+    with open(f"dns-servers/{item}" , 'rb') as file:
+        obj_list.append(pk.load(file))
 
-
+    
+print("---------------------------------------------")
+counter = 0
+for item in obj_list :
+   
+    counter += 1
+    print(f"{counter}- {item.name}:     {item.ip}")
