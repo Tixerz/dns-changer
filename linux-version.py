a9f1dd5d13da22d1b11ea717eb652b155a230dbf
Tixerz <petete1296@gmail.com>
2023-10-19 00:12:25 +0330

    add server update

--- linux-version.py
+++ linux-version.py
@@ -8,17 +8,6 @@ class dns:
         with open(f"dns-servers/{self.name}" , "wb") as f :
             pk.dump(self , f)
         
-    def remove(self):
-        os.remove(f"dns-servers/{self.name}")
-        with open("/etc/resolv.conf","r") as f:
-            lines=f.readlines()
-            print(lines)
-        with open("/etc/resolv.conf","w")as f:
-            for l in lines:
-                if l != f'nameserver {self.ip}':
-                    f.write(l)
-
-    
     def write(self):
         with open("/etc/resolv.conf" , "w") as file :
             file.write(f'\nnameserver {self.ip}')
